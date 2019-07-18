43fb48df86f4ac41b36a50faeae3eabee03e79c6
vihwan <vihwan@ajou.ac.kr>
2019-07-18 16:58:36 +0900

    Use Arrow Function in ES6 Script

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,61 @@
 import React from 'react';
+import logo from '../logo.svg';
 
-function Header(){
+
+//Arrow Function in ES6
+//Stateless component , dump component
+//Lifecycle을 타지 않음 
+const Header = props => { //이런식으로 하면 String 값이 아닌 Object 값으로 온다
+	console.log(props); //props는 하나의 매개변수로 받아온다.
+	console.log(props.name) //사용할 때에는 이렇게 쓰기.
+	console.log(props.foo)
+
+	// ES5
+	// const name = props.name;
+	// const foo = props.foo;
+
+	// ES6
+	 const{name,foo} = props;
+
 	return (
-		<div>Hello I'm a header</div>
-		)	
+		<header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+		<div>
+			My name is {name}. {foo}
+		</div>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+	)	
 }
+
+
+// function Header(){
+// 	return (
+// 		<header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+// 	)	
+// }
 
 export default Header;
