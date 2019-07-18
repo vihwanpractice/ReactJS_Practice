d20113df19806ad8a63db6f7eeb2557c5857dc82
vihwan <vihwan@ajou.ac.kr>
2019-07-18 16:19:09 +0900

    Initialize React.js Setting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './components/header'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
