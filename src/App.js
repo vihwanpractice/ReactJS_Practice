a34c1ae0c0c8e20185084e5468fd9b8c844ccfa9
vihwan <vihwan@ajou.ac.kr>
2019-07-18 16:56:53 +0900

    Modify ES5 Script to ES6 Script

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './components/header'
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+//ES6
+class App extends Component{
+  componentDidMount() {
+    console.log('Component DID MOUNT')
+  }
+  render(){
+    const name = "Rain"
+    return (
+      <div className="App">
+        <Header name = {name} foo = "bar"/>
+      </div>
+    );
+  }
 }
 
 export default App;
